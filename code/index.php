d8f1413e0cbe4ea6ad85d3f0fdb55ede7bdad592
matt bee <72115097+mbluelander@users.noreply.github.com>
2025-04-30 19:36:28 -0400

    Add files via upload

---
+++ code/index.php
@@ -0,0 +1,83 @@
+<?
+// scripts for all js functions. To be placed at the bottom of index.php ///
+
+<script>
+// builds text to be shared by sharebutton. If an element on the page has the "pagetitle" attribute, it uses that text + the URL, otherwise it just uses the URL. //
+var url = window.location.href;
+
+if (document.getElementById("pagetitle"))  {
+var title = document.getElementById("pagetitle").textContent + " ";
+} else {
+var title = "";}
+
+title += url ;
+
+// Puts the text to share in a hidden text field //
+
+document.getElementById("sharebutton").value = title;
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("sharebutton");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+  
+  // Alert the copied text
+  alert("Copied: " + copyText.value);
+}
+
+// "Like button" - displays a message, with different messages if clicked multiple times //
+let x = 1
+function likebutton() {
+const y = x++
+if (y == 1) {
+	alert("Thank you! You have clicked this button once.");
+	}
+if (y > 1 && y < 11) {
+	alert("Thank you! You have clicked this button " + y + " times.");
+	}
+if (y > 10 && y < 16) {
+	alert("Wow! You REALLY like this page! Thank you so much!");
+	}
+if (y == 15) {
+	alert("?button malfunction");
+	}
+}
+</script>
+<script>
+// When the user clicks on the button, 
+toggle between hiding and showing the dropdown content //
+function showForm() {
+document.getElementById("replyform").classList.toggle("show");
+}
+
+function dropFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Remove focus from dropdown button if the user clicks it again //
+// Close the dropdown if the user clicks outside of it //
+
+window.onclick = function(event) {
+  if (event.target.matches('.dropbtn') && !document.getElementById("myDropdown").classList.contains('show')) {
+
+  event.target.blur();
+  }
+
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+</script>
+?>
---
+++ code/page.php
@@ -0,0 +1,39 @@
+<?
+// Custom dynamic variables. Place at bottom of section beginning: //
+//////// Setup Page Variables and Text ///////////
+
+/// subject - if URL has ?subject=[x], [x] is stored in this dynamic variable ///
+
+if (isset($_GET["subject"])) {
+	$dynamic_content["subject"] = $_GET["subject"];
+} else {
+$dynamic_content["subject"] = "";
+}	
+
+/// likebutton - does nothing but show a fun Easter egg message on multiple clicks. See JavaScript in index.php ///
+
+$dynamic_content["likebutton"] = "<button onClick=likebutton()  class='sharebutton'>Like &#128077;</button>";
+
+/// sharebutton - uses JavaScript in index.php to copy page title and/or URL to clipboard ///
+
+
+$dynamic_content["sharebutton"] = "<input id='sharebutton' type='text' value=' ' id='myInput' style='display:none;'>";
+
+$dynamic_content["sharebutton"] .= "<button onClick='myFunction()' class='sharebutton'>Share &#x1F4E3;</button>";
+
+/// replybutton - reveals hidden div with form to submit a pingback url, with link to launch full contact form. Relies on external PHP mailsend function. Passes the title of the current page to the ?subject= query ///
+
+$dynamic_content["replybutton"] = "<button onClick='showForm()' class='sharebutton'>Reply &#x1f48c;</button><div class='replyhidden' id='replyform'><blockquote>If you mentioned this post on your own site and would like to notify me, please enter the URL here:<br>
+<form action='/contact-php/mail.php' method='POST'><input type='text' name='name' id='name' style='width:30em'; required><input type='hidden' name='subject' value='" . $dynamic_content["title"] . "'> <input type='hidden' name='email'><input type='hidden' name='message' value='Mention'><input type='text' name='captcha' style='display:none;'> <input type='submit' value='Send'></form><br>Otherwise, click <a href='https://mattbee.zone/contact?subject=" . $dynamic_content["title"] . "'>here</a> to send a message.</blockquote></div>"; 
+
+/// pagebuttons - combine three previous buttons into one function ///
+
+$dynamic_content["pagebuttons"] = $dynamic_content["likebutton"] . " " .  $dynamic_content["sharebutton"] . " " .  $dynamic_content["replybutton"];
+
+/// pat, nb, lander - custom emoji ///
+
+$dynamic_content["pat"] = "<span class='emoji' style='background: no-repeat center url(/emoji/headpat.png); background-position:bottom; background-size: 20px 20px;'> </span>";
+
+$dynamic_content["lander"] = "<span class='emoji' style='background: no-repeat center url(/emoji/lander.png); background-position:bottom; background-size: 20px 20px;'> </span>";
+
+$dynamic_content["nb"] = "<span class='emoji' style='background: no-repeat center url(/emoji/nb.png); background-position:bottom; background-size: 20px 20px;'> </span>";
